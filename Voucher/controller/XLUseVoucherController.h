97ec9992878ac704cbd432c174e82d2d86a312cc
xieliang <xieliang43@126.com>
2012-07-29 17:43:27 +0800

    12qwe

---
+++ Voucher/controller/XLUseVoucherController.h
@@ -0,0 +1,13 @@
+//
+//  XLUseVoucherController.h
+//  Voucher
+//
+//  Created by xie liang on 12-7-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLUseVoucherController : UIViewController
+
+@end
